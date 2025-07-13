4647bb6fb715bdb6c72d0d86e14fbd4c095b087d
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2025-07-13 18:19:07 +0400

    Add missing import for datetime in log_queries.py

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,6 +1,7 @@
 import sqllite3
 import functools
 import logging
+from datetime import datetime
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
